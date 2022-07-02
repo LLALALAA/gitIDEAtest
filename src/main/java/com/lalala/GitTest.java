517322bae7ac6917ad2e3c4d9cbc8c36a09af9e5
LALALA <2274364071@qq.com>
2022-07-02 14:47:32 +0800

    Second IDEA commit

--- src/main/java/com/lalala/GitTest.java
+++ src/main/java/com/lalala/GitTest.java
@@ -2,6 +2,8 @@ package com.lalala;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,git!");
+        System.out.println("hello,git!111");
+        System.out.println("hello,git!222");
+
     }
 }
